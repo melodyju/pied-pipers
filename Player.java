c3d5fd9b56181ca2e4e627df69c11913e2c5c066
rituparnamukherjee <ritumukh09@gmail.com>
2014-09-09 18:47:13 -0400

    Player.java file edited

--- Player.java
+++ Player.java
@@ -1,4 +1,4 @@
-package piedpipers.firstTry;
+package piedpipers.group1;
 
 import java.util.*;
 
@@ -11,7 +11,10 @@ public class Player extends piedpipers.sim.Player {
 	static double mpspeed = 0.09;
 
 	static int magnet = 0;
-	static Point magnetLocation = new Point(dimension/2, dimension/2); //assume there is only one magnet
+	static double[] angle;
+	private boolean comeback;
+	public Point gateLocation;
+	public Point magnetLocation;
 
 	public Player() {
 		super();
@@ -33,11 +36,44 @@ public class Player extends piedpipers.sim.Player {
 
 	public Point move(Point[] pipers, Point[] rats) {
 		npipers = pipers.length;
-
+		System.out.println(npipers);
+		double fraction = 360/npipers;
+		angle = new double[npipers];
+		for(int i=0 ; i< npipers; i++)
+		{
+			System.out.println("In the loop..!!");
+			angle[i] = fraction*i;
+			System.out.println(angle[i]);
+		}
+		System.out.println("Done..!!");
+		gateLocation = new Point(dimension/2, dimension/2); //assume there is only one magnet
+		magnetLocation = new Point(3*dimension/4, dimension/2);
 		Point current = pipers[id];
-
+		System.out.println("PRINTING MAGNET LOCATION....." + magnetLocation.x +  "," + magnetLocation.y);
+		
+		if(current.x < magnetLocation.x){
+			this.music = false;
+			double dist = distance(current, magnetLocation);
+			double ox = (magnetLocation.x - current.x) / dist * pspeed;
+			double oy = (magnetLocation.y - current.y) / dist * pspeed;
+			current.x += ox;
+			current.y += oy;
+			return current;
+		}
+		/*
+		if(current.x < gateLocation.x){
+			this.music = false;
+			double dist = distance(current, gateLocation);
+			double ox = (gateLocation.x - current.x) / dist * pspeed;
+			double oy = (gateLocation.y - current.y) / dist * pspeed;
+			current.x += ox;
+			current.y += oy;
+			return current;
+		}
+		*/
 		if (this.id == magnet) {
-			if (!current.equals(magnetLocation)) {
+			if (current.x < magnetLocation.x)
+				{
 				this.music = false;
 				double dist = distance(current, magnetLocation);
 				double ox = (magnetLocation.x - current.x) / dist * pspeed;
@@ -53,7 +89,22 @@ public class Player extends piedpipers.sim.Player {
 		}
 
 		else {
-			return current; //change later
+			if (comeback){
+				this.music = true;
+				if (true) { //close enuf to magnet to drop off all rats
+					comeback = false;
+				}
+				return current;
+			}
+			else {
+				this.music = false;
+				current.x += pspeed * Math.sin(angle[id] * Math.PI / 180);
+				current.y += pspeed * Math.cos(angle[id] * Math.PI / 180);
+				if (false) { //implement condition later
+					comeback = true;
+				}
+				return current; //change later
+			}
 		}
 	}
 
